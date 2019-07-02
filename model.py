e3fd44e70dcb5a4a7a92abda3e379aa0e06f9bc3
Hyunseung Yoo <hsyoo@anl.gov>
2019-07-02 10:52:21 -0500

    init version of model and train script

---
+++ model.py
@@ -0,0 +1,79 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras import backend as K
+
+def build_model(feature_shapes, input_features, params):
+    input_models = {}
+    dropout_rate = params.get('drop')
+    for fea_type, shape in feature_shapes.items():
+        base_type = fea_type.split('.')[0]
+        if base_type in ['cell', 'drug']:
+            box = build_feature_model(input_shape=shape, name=fea_type,
+                                      dense_layers=params.get('dense_feature_layers'),
+                                      dropout_rate=dropout_rate)
+            input_models[fea_type] = box
+
+    inputs = []
+    encoded_inputs = []
+    for fea_name, fea_type in input_features.items():
+        shape = feature_shapes[fea_type]
+        fea_input = Input(shape, name='input.' + fea_name)
+        inputs.append(fea_input)
+        if fea_type in input_models:
+            input_model = input_models[fea_type]
+            encoded = input_model(fea_input)
+        else:
+            encoded = fea_input
+        encoded_inputs.append(encoded)
+
+    merged = concatenate(encoded_inputs)
+
+    h = merged
+    for i, layer in enumerate(params.get('dense')):
+        x = h
+        h = Dense(layer, activation=params.get('activation'))(h)
+        if dropout_rate > 0:
+            h = PermanentDropout(dropout_rate)(h)
+        if params.get('residual'):
+            try:
+                h = tf.keras.layers.add([h, x])
+            except ValueError:
+                pass
+    output = Dense(1)(h)
+
+    return Model(inputs, output)
+
+def build_feature_model(input_shape,
+                        name='',
+                        dense_layers=[1000, 1000, 1000],
+                        activation='relu',
+                        residual=False,
+                        dropout_rate=0):
+    x_input = Input(shape=input_shape)
+    h = x_input
+    for i, layer in enumerate(dense_layers):
+        x = h
+        h = Dense(layer, activation=activation)(h)
+        if dropout_rate > 0:
+            h = PermanentDropout(dropout_rate)(h)
+        if residual:
+            try:
+                h = tf.keras.layers.add([h, x])
+            except ValueError:
+                pass
+    model = Model(x_input, h, name=name)
+    return model
+
+
+class PermanentDropout(Dropout):
+    def __init__(self, rate, **kwargs):
+        super(PermanentDropout, self).__init__(rate, **kwargs)
+        self.uses_learning_phase = False
+
+    def call(self, x, mask=None):
+        if 0. < self.rate < 1.:
+            noise_shape = self._get_noise_shape(x)
+            x = K.dropout(x, self.rate, noise_shape)
+        return x
---
+++ train.py
@@ -0,0 +1,41 @@
+from model import build_model
+# from data import UnoDataLoader
+import pandas as pd
+
+def run_cv_training(cv, dropout):
+    (df_y_train, df_x_train_cl, df_x_train_dr), (df_y_val, df_x_val_cl, df_x_val_dr) = load_data()
+    feature_shapes = {'cell.geneGE': (1927,), 'drug.dd': (4392,)}
+    input_features = {'cell.geneGE': 'cell.geneGE', 'drug.dd': 'drug.dd'}
+    args = {'drop': 0,
+            'activation': 'relu',
+            'dense_feature_layers': [1000, 1000, 1000],
+            'dense': [1000, 1000, 1000],
+            'residual': False}
+    model = build_model(feature_shapes, input_features, args)
+    model.summary()
+    model.compile(loss='mse', optimizer='adam')
+    model.fit([df_x_train_cl, df_x_train_dr], df_y_train,
+              batch_size=512,
+              epochs=50,
+              validation_data=([df_x_val_cl, df_x_val_dr], df_y_val)
+              )
+
+def load_data():
+    store = pd.HDFStore('cv.h5', 'r')
+    df_y_train = store.get('df_y_train')
+    df_x_train_cl = store.get('df_x_train_cl')
+    df_x_train_dr = store.get('df_x_train_dr')
+    df_y_val = store.get('df_y_val')
+    df_x_val_cl = store.get('df_x_val_cl')
+    df_x_val_dr = store.get('df_x_val_dr')
+
+    return (df_y_train, df_x_train_cl, df_x_train_dr), (df_y_val, df_x_val_cl, df_x_val_dr)
+
+def main():
+    for cv in range(0, 9):
+        for dropout in range(0, 0.5, 0.1):
+            run_cv_training(cv, dropout)
+
+if __name__ == '__main__':
+    # main()
+    run_cv_training(0, 0.1)
